5aa2da1faff52ac9abefd61d19ae18e1e7c6266c
萌萌哒帕克 <35246761+adorableparker@users.noreply.github.com>
2021-12-03 17:34:33 +0800

    准备对接
    Signed-off-by: 萌萌哒帕克 <35246761+AdorableParker@users.noreply.github.com>

--- bot/src/main/java/com/example/nymph_TB_DLC/DLC.kt
+++ bot/src/main/java/com/example/nymph_TB_DLC/DLC.kt
@@ -7,16 +7,10 @@
 package com.example.nymph_TB_DLC
 
 
-import com.example.nymph_TB_DLC.MirrorWorldConfig.ExchangeRate
+
 import kotlinx.coroutines.cancel
-import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
-import net.mamoe.mirai.console.command.CompositeCommand
-import net.mamoe.mirai.console.command.MemberCommandSenderOnMessage
-import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import net.mamoe.mirai.event.events.GroupMessageEvent
-import net.mamoe.mirai.event.nextEvent
 
 
 object DLC : KotlinPlugin(
@@ -24,167 +18,14 @@ object DLC : KotlinPlugin(
         id = "MCP.TB_DLC",
         version = "0.0.1",
         name = "TB_DLC-MirrorWorld"
-    ) { dependsOn("MCP.navigatorTB_Nymph") }) {
-
+    ) { dependsOn("MCP.navigatorTB_Nymph", "0.15.01") }) {
 
     override fun onEnable() {
-        // 从数据库自动读
         MirrorWorldUser.reload()
         MirrorWorldAssets.reload()
-
-        PermanentInfo.register()  //用户信息
-        PermanentBuild.register() //建立角色
-        APAllot.register()        //分配点数
     }
 
     override fun onDisable() {
-
-
         DLC.cancel()
     }
 }
-
-
-/** DLC所含有的命令
- * 需要在主体初始化时加载
- * */
-annotation class DLCCommand
-
-private suspend fun MemberCommandSenderOnMessage.yesOrNo(theme: String, no: String): Boolean {
-    sendMessage("[10秒/3次]$theme<是/否>")
-    for (i in 0..2) {
-        when (nextEvent<GroupMessageEvent>(10_000) { it.sender == this.user }.message.contentToString()) {
-            "否" -> {
-                sendMessage(no)
-                return true
-            }
-            "是" -> break
-            else -> if (i > 0) sendMessage("[20秒/${i}次]$theme<是/否>")
-        }
-    }
-    return false
-}
-
-@DLCCommand
-object PermanentInfo : SimpleCommand(
-    DLC, "我的信息",
-    description = "用户信息"
-) {
-    @Handler
-    suspend fun MemberCommandSenderOnMessage.main() {
-        sendMessage("测试通过")
-        val userData = MirrorWorldUser.userPermanent.getOrPut(user.id) {
-            PermanentData()
-        }
-        sendMessage(userData.outInfo())
-    }
-}
-
-@DLCCommand
-object PermanentBuild : SimpleCommand(
-    DLC, "建立角色",
-    description = "玩家角色建立"
-) {
-    @Handler
-    suspend fun MemberCommandSenderOnMessage.main() {
-        sendMessage("测试开始")
-        val userData = MirrorWorldUser.userPermanent.getOrPut(user.id) {
-            PermanentData()
-        }
-        if (userData.pc != null) {
-            if (yesOrNo("创建新角色？\n清除存档,不可恢复", "角色建立取消")) return
-        }
-        userData.pc = PlayerCharacter()
-        sendMessage(
-            """
-        角色已建立,可以开始分配属性点
-        现在拥有${userData.pc!!.showAP()}点属性点可分配至
-        力量：与物攻相关
-        法力：与法术相关
-        智力:与经验获取,战斗效率相关
-        体质：与生命值和法力值相关
-        速度：与行动速度相关
-        运气：与事件判定相关
-        六项基本属性,确定属性前随时可以查看并改变
-        """.trimIndent()
-        )
-    }
-}
-
-@DLCCommand
-object APAllot : CompositeCommand(
-    DLC, "属性点",
-    description = "分配属性点"
-) {
-    @SubCommand("分配")
-    suspend fun MemberCommandSenderOnMessage.main() {
-        sendMessage("测试开始")
-        val userData = MirrorWorldUser.userPermanent.getOrPut(user.id) {
-            PermanentData()
-        }
-        // 判断角色是否存在
-        if (userData.pc == null) {
-            sendMessage("角色不存在，请建立角色后操作")
-            return
-        }
-        // 判断是否可分配点数
-        val ap = userData.pc!!.showAP()
-        if (ap == 0) {
-            sendMessage("无可分配点数")
-            return
-        }
-        // 开始分配点数
-        sendMessage("[30秒]请按照以下顺序输入点数分配方案(各项之间使用冒号分割)：\n力量：法力：智力:体质：速度：运气")
-        val lt = arrayOf(6)
-        val enter = false
-        GetPlan@ while (!enter) {
-            // 获取用户输入
-            val plan = nextEvent<GroupMessageEvent>(30_000) { it.sender == user }.message.contentToString()
-                .split(":", "：", limit = 7)
-            // 判断输入数量
-            if (plan.size < 6) {
-                sendMessage("[30秒]参数数量不匹配,请重新输入")
-                continue
-            }
-            // 按顺序分配
-            for (i in 0..5) {
-                when (val apv = plan[i].toIntOrNull()) { //检查输入合法性
-                    null -> {
-                        sendMessage("属性值必须为整数")
-                        continue@GetPlan
-                    }
-                    in 0..7 -> lt[i] = apv
-                    else -> {
-                        sendMessage("[30秒]每项最多为其分配7点,请重新输入")
-                        continue@GetPlan
-                    }
-                }
-            }
-            // 检查点数用量
-            if (ap < lt.sum()) {
-                sendMessage("该方案所用点数超出可用点数总量,请重新输入")
-            }
-            // 输出汇报
-            val mod = Tool(lt)
-            sendMessage(
-                """
-                方案有效,生成角色属性预览如下
-                等级: 1
-                HP:${mod.draftHP()}\tMP:${mod.draftMP()}
-                ATK:${mod.draftATK()}\tMAT:${mod.draftMAT()}
-                ${mod.show6D()}
-                """.trimIndent()
-            )
-        }
-        // 用户确定
-        if (yesOrNo(
-                "确定当前方案？（剩余未分配的属性点将会以1属性点:${ExchangeRate}金币的比例自动转化为金币值）\n确定属性后,不可再次更改",
-                "点数分配取消"
-            )
-        ) return
-
-        userData.pc!!.set6D(lt)
-        sendMessage("点数设定完成,可进行特性/技能学习")
-    }
-}
-
--- gradlew.bat
+++ gradlew.bat
@@ -1,11 +1,11 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  Windows 的 Gradle 启动脚本
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
+@rem 使用 windows NT shell 为变量设置本地范围
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
@@ -13,10 +13,10 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem 在此处添加默认 JVM 选项。 您还可以使用 JAVA_OPTS 和 GRADLE_OPTS 将 JVM 选项传递给此脚本。
 set DEFAULT_JVM_OPTS=
 
-@rem Find java.exe
+@rem 查找 java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
@@ -24,10 +24,10 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo 错误：未设置 JAVA_HOME 并且在您的 PATH 中找不到“java”命令。
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo 请在您的环境中设置 JAVA_HOME 变量以匹配
+echo Java 安装的位置。
 
 goto fail
 
@@ -38,10 +38,10 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto init
 
 echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo 错误：JAVA_HOME 设置为无效目录：%JAVA_HOME%
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo 请在您的环境中设置 JAVA_HOME 变量以匹配
+echo Java 安装的位置。
 
 goto fail
 
