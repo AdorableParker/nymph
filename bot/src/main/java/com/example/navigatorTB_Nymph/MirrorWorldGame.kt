768606b7db2fa465146e53f8b62dbb2366890c41
萌萌哒帕克 <35246761+adorableparker@users.noreply.github.com>
2021-12-08 13:33:32 +0800

    更新DLC
    Signed-off-by: 萌萌哒帕克 <35246761+AdorableParker@users.noreply.github.com>

--- bot/src/main/java/com/example/navigatorTB_Nymph/MirrorWorldGame.kt
+++ bot/src/main/java/com/example/navigatorTB_Nymph/MirrorWorldGame.kt
@@ -1,6 +1,6 @@
 package com.example.navigatorTB_Nymph
 
-import com.example.nymph_TB_DLC.MirrorWorld
+import com.nymph_TB_DLC.MirrorWorld
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.command.MemberCommandSenderOnMessage
@@ -12,16 +12,12 @@ object MirrorWorldGame {
     fun register() {
         PlayerInfo.register()
         PlayerBuild.register()
-        APAllot.register()
-        CCareer.register()
         PvPBattle.register()
     }
 
     fun unregister() {
         PlayerInfo.unregister()
         PlayerBuild.unregister()
-        APAllot.unregister()
-        CCareer.unregister()
         PvPBattle.unregister()
     }
 
@@ -52,31 +48,6 @@ object MirrorWorldGame {
         }
     }
 
-    object APAllot : SimpleCommand(
-        PluginMain, "APAllot", "属性点分配",
-        description = "分配属性点"
-    ) {
-        @Handler
-        suspend fun MemberCommandSenderOnMessage.main() {
-            if (group.botMuteRemaining > 0) return
-            if (PluginMain.DLC_MirrorWorld) {
-                MirrorWorld().apAllotted(this)
-            } else sendMessage("缺少依赖DLC")
-        }
-    }
-
-    object CCareer : SimpleCommand(
-        PluginMain, "Career", "选择职业",
-        description = "角色职业选择"
-    ) {
-        @Handler
-        suspend fun MemberCommandSenderOnMessage.main() {
-            if (group.botMuteRemaining > 0) return
-            if (PluginMain.DLC_MirrorWorld) {
-                MirrorWorld().chooseCareer(this)
-            } else sendMessage("缺少依赖DLC")
-        }
-    }
 
     object PvPBattle : SimpleCommand(
         PluginMain, "PvP", "玩家对战",
@@ -87,6 +58,19 @@ object MirrorWorldGame {
             if (group.botMuteRemaining > 0) return
             if (PluginMain.DLC_MirrorWorld) {
                 MirrorWorld().pvp(this, user)
+            } else sendMessage("缺少依赖DLC")
+        }
+    }
+
+    object PlayerTransfer : SimpleCommand(
+        PluginMain, "TransferP", "金币转账",
+        description = "玩家金币转移"
+    ) {
+        @Handler
+        suspend fun MemberCommandSenderOnMessage.main(user: User, amount: Int) {
+            if (group.botMuteRemaining > 0) return
+            if (PluginMain.DLC_MirrorWorld) {
+                MirrorWorld().transfer(this, user, amount)
             } else sendMessage("缺少依赖DLC")
         }
     }
